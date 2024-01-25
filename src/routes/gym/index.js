e3c2dd6b285f6a8186cb12997adfbedbfb4df7a4
Diogo Felipe <dlcfelipp@gmail.com>
2024-01-25 14:36:47 -0300

    this correction

--- src/routes/gym/index.js
+++ src/routes/gym/index.js
@@ -6,23 +6,23 @@ class GymsRouter {
 
     this.gymsController = gymsController;
 
-    gyms.get("/:cnpj", (request, response) => {
+    this.gyms.get("/:cnpj", (request, response) => {
       this.gymsController.index(request, response);
     });
 
-    gyms.get("/", (request, response) => {
+    this.gyms.get("/", (request, response) => {
       this.gymsController.getAll(request, response);
     });
 
-    gyms.post("/", (request, response) => {
+    this.gyms.post("/", (request, response) => {
       this.gymsController.create(request, response);
     });
 
-    gyms.delete("/:cnpj", (request, response) => {
+    this.gyms.delete("/:cnpj", (request, response) => {
       this.gymsController.delete(request, response);
     });
 
-    gyms.put("/:cnpj", (request, response) => {
+    this.gyms.put("/:cnpj", (request, response) => {
       this.gymsController.update(request, response);
     });
   }
